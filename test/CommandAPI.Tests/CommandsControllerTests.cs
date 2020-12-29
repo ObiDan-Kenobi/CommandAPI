a3fa582154777ac6d62072694e54ee337373006d
Dan Spacil <danielspacil@outlook.com>
2020-12-29 17:13:53 +0100

    Testing failing test project

--- test/CommandAPI.Tests/CommandsControllerTests.cs
+++ test/CommandAPI.Tests/CommandsControllerTests.cs
@@ -294,7 +294,8 @@ namespace CommandAPI.Tests
             //Act
             var result = controller.DeleteCommand(0);
             //Assert
-            Assert.IsType<NotFoundResult>(result);
+            // Assert.IsType<NotFoundResult>(result);
+            Assert.IsType<OkResult>(result);
         }
 
     }
